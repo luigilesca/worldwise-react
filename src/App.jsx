a6577e0760000746dd8465ae103b75ee4ea101c4
Luigi Lesca <luigilesca@hotmail.it>
2024-03-04 15:47:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// Components
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        {/* <PageNav /> */}
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='product' element={<Product />} />
+          <Route path='pricing' element={<Pricing />} />
+          <Route path='login' element={<Login />} />
+          <Route path='app' element={<AppLayout />}>
+            <Route index element={<p>List of cities</p>} />
+            <Route path='cities' element={<p>List of cities</p>} />
+            <Route path='countries' element={<p>Countries</p>} />
+            <Route path='form' element={<p>Form</p>} />
+          </Route>
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App;
